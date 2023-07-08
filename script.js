928dbddd7b9ceeffb45921ce578bacec0cc0b73d
Eden Hazani <mr.edenhazani@gmail.com>
2023-07-08 15:55:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const urlParams = new URLSearchParams(window.location.search);
+console.log(urlParams);
 if(urlParams.has("joinAdventureIdentifier")) {
     const joinAdventureIdentifier = urlParams.get("joinAdventureIdentifier");
     window.location.replace(`com.dncreate://`);
