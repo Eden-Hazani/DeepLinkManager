5d5eec93c6538dba8f21ef6a7338fd4bcdf14845
Eden Hazani <mr.edenhazani@gmail.com>
2023-07-12 19:30:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const urlParams = new URLSearchParams(window.location.search);
 if(urlParams.has("joinAdventureIdentifier")) {
     const joinAdventureIdentifier = urlParams.get("joinAdventureIdentifier");
     try{
-        window.location.replace(`com.dncreate://Adventures/JoinAdventure/${joinAdventureIdentifier}`);
+        window.location.replace(`com.dncreate://adventures/JoinAdventure/${joinAdventureIdentifier}`);
     }catch (e){
         prompt("Error", e)
     }
